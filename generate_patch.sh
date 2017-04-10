9e5e9a3c6d3d513150cb635cb2370bfcf2d367fb
Ziyue Yang <yzylivezh@hotmail.com>
2017-04-10 11:16:51 +0800

    add system service and view event notification

---
+++ generate_patch.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+FRAMEWORK_BASE="/mnt/data/aosp-latest/aosp/frameworks/base"
+SEPOLICY="/mnt/data/aosp-latest/aosp/external/sepolicy"
+PATCH_DIR="/home/yzy/2017_spring/DroidCC"
+
+gen_patch () {
+	cd $1
+	git checkout android-6.0.1_r77
+	git checkout -b access_control_patch
+	git merge android-6.0.1_r77_access_control --squash
+	git add -all .
+	git commit -m "access control patch"
+	git format-patch android-6.0.1_r77
+	git checkout android-6.0.1_r77_access_control
+	git branch -D access_control_patch
+	mv 000* "$PATCH_DIR/access_control_$2.patch"
+}
+
+gen_patch $FRAMEWORK_BASE "FRAMEWORK_BASE"
+gen_patch $SEPOLICY "SEPOLICY"
