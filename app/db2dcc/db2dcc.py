fceb44478695a919c56afa3411111f5ba901ede0
Ziyue Yang <yzylivezh@hotmail.com>
2017-12-26 13:57:15 +0800

    update according to droidbot update

--- app/db2dcc/db2dcc.py
+++ app/db2dcc/db2dcc.py
@@ -52,6 +52,7 @@ def assemble_perm_rules(apk_data_path_list, output_path, exclude_activities, psc
         # start_state :                       [app   , ...]
 
         package_name = apk_data_path.split("/")[-1]
+        print(package_name)
 
         def load_jsons(json_path_list):
             json_list = []
@@ -66,23 +67,27 @@ def assemble_perm_rules(apk_data_path_list, output_path, exclude_activities, psc
                 dumpsys_lines = dumpsys_file.readlines()
 
                 granted_perms = set()
-                line_idx = len(dumpsys_lines) - 1
-                while dumpsys_lines[line_idx].strip() != "grantedPermissions:":
+                line_idx = 0
+                while dumpsys_lines[line_idx].strip() != "requested permissions:":
+                    line_idx += 1
+                line_idx += 1
+                while dumpsys_lines[line_idx].strip() != "install permissions:":
                     granted_perms.add(dumpsys_lines[line_idx].strip())
-                    line_idx -= 1
+                    line_idx += 1
 
+                # TODO: filter background services
                 trace_perm_list = []
                 for trace_path in trace_path_list:
                     curr_perm_set = set()
-                    with open(trace_path, "r") as trace_file:
+                    with open(trace_path, "rb") as trace_file:
                         trace_lines = trace_file.readlines()
                     if len(trace_lines):
                         line_idx = 0
-                        while trace_lines[line_idx].strip() != "*methods":
+                        while trace_lines[line_idx].strip() != b"*methods":
                             line_idx += 1
                         line_idx += 1
-                        while trace_lines[line_idx].strip() != "*end":
-                            fields = trace_lines[line_idx].split("\t")
+                        while trace_lines[line_idx].strip() != b"*end":
+                            fields = trace_lines[line_idx].decode().split("\t")
                             method_key = "%s %s" % (fields[2], fields[3])
                             if method_key in pscout_map:
                                 curr_perm_set |= pscout_map[method_key] & granted_perms
@@ -93,20 +98,20 @@ def assemble_perm_rules(apk_data_path_list, output_path, exclude_activities, psc
         event_path_pair = ("%s/events/event_" % apk_data_path, ".json")
         trace_path_pair = ("%s/events/event_trace_" % apk_data_path, ".trace")
         state_path_pair = ("%s/states/state_" % apk_data_path, ".json")
-        screenshot_path_pair = ("%s/states/screenshot_" % apk_data_path, ".png")
+        screenshot_path_pair = ("%s/states/screen_" % apk_data_path, ".jpg")
 
         event_tag_list = [x[len("event_"):-len(".json")]
-                          for x in os.walk("%s/events" % apk_data_path).next()[2]
+                          for x in next(os.walk("%s/events" % apk_data_path))[2]
                           if x.endswith(".json")]
         trace_tag_list = [x[len("event_trace_"):-len(".trace")]
-                          for x in os.walk("%s/events" % apk_data_path).next()[2]
+                          for x in next(os.walk("%s/events" % apk_data_path))[2]
                           if x.endswith(".trace")]
         state_tag_list = [x[len("state_"):-len(".json")]
-                          for x in os.walk("%s/states" % apk_data_path).next()[2]
+                          for x in next(os.walk("%s/states" % apk_data_path))[2]
                           if x.endswith(".json")]
-        screenshot_tag_list = [x[len("screenshot_"):-len(".png")]
-                               for x in os.walk("%s/states" % apk_data_path).next()[2]
-                               if x.endswith(".png")]
+        screenshot_tag_list = [x[len("screen_"):-len(".jpg")]
+                               for x in next(os.walk("%s/states" % apk_data_path))[2]
+                               if x.endswith(".jpg")]
 
         common_tags = list(set(event_tag_list) & set(trace_tag_list) &
                            set(state_tag_list) & set(screenshot_tag_list))
@@ -149,18 +154,17 @@ def assemble_perm_rules(apk_data_path_list, output_path, exclude_activities, psc
 
             if event["event"]["event_type"] == "touch":
                 view_ctx_str = "activity=%s;package=%s;view_action_id=0" % (activity, package_name)
-                x = event["event"]["x"]
-                y = event["event"]["y"]
                 this_view = None
                 for view in state["views"]:
-                    if view["bounds"][0][0] <= x <= view["bounds"][1][0] and \
-                       view["bounds"][0][1] <= y <= view["bounds"][1][1]:
+                    if match_touched_view(event, view):
                        # pre-order walking
                         this_view = view
                 if this_view is None:
                     continue
             elif event["event"]["event_type"] == "key" and \
                  event["event"]["name"] == "BACK":
+                if not len(state["views"]):
+                    continue
                 view_ctx_str = "activity=%s;package=%s;view_action_id=1" % (activity, package_name)
                 this_view = state["views"][0]
                 for view in state["views"]:
@@ -199,10 +203,10 @@ def assemble_perm_rules(apk_data_path_list, output_path, exclude_activities, psc
         # output screenshots
         for activity in ui_perm_rules[package_name]:
             for view_info in ui_perm_rules[package_name][activity]:
-                png_path = view_info.pop("screenshotPath")
-                png_tag = view_info["eventTag"]
+                jpg_path = view_info.pop("screenshotPath")
+                jpg_tag = view_info["eventTag"]
                 bounds = view_info["bounds"]
-                im = Image.open(png_path)
+                im = Image.open(jpg_path)
                 draw = ImageDraw.Draw(im)
                 for i in range(-5, 5):
                     draw.rectangle([
@@ -210,8 +214,8 @@ def assemble_perm_rules(apk_data_path_list, output_path, exclude_activities, psc
                         bounds[1][0] + i, bounds[1][1] + i
                     ], outline=(153, 204, 51, 255 + 10 * (i - 5)))
                 del draw
-                with open("%s/screenshots/%s.png" % (output_path, png_tag), "wb") as out_png_file:
-                    im.save(out_png_file)
+                with open("%s/screenshots/%s.jpg" % (output_path, jpg_tag), "wb") as out_jpg_file:
+                    im.save(out_jpg_file)
 
         # output perm rules
         with open("%s/perm_rules/%s.json" % (output_path, package_name), "w") as output_file:
@@ -219,6 +223,20 @@ def assemble_perm_rules(apk_data_path_list, output_path, exclude_activities, psc
                 "start_perm_rules": start_perm_rules[package_name],
                 "ui_perm_rules": ui_perm_rules[package_name]
             }, output_file, indent=2)
+
+
+def match_touched_view(touch_event, view):
+    x = touch_event["event"]["x"]
+    y = touch_event["event"]["y"]
+    bounds = touch_event["event"]["view"]["bounds"]
+    if bounds is not None and \
+       bounds == view["bounds"]:
+        return True
+    if x is not None and y is not None and \
+       view["bounds"][0][0] <= x <= view["bounds"][1][0] and \
+       view["bounds"][0][1] <= y <= view["bounds"][1][1]:
+        return True
+    return False
 
 
 def run(config_json_path):
@@ -234,7 +252,7 @@ def run(config_json_path):
     output_path = os.path.abspath(config_json["output_dir"])
     exclude_activities = config_json["exclude_activities"]
     apk_data_path_list = ["%s/%s" % (droidbot_out_path, x)
-                          for x in os.walk(droidbot_out_path).next()[1]]
+                          for x in next(os.walk(droidbot_out_path))[1]]
 
     assemble_perm_rules(apk_data_path_list, output_path, exclude_activities, pscout_map)
 
